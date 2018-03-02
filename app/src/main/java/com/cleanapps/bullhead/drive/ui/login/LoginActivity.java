6705501aeaa42d345069a97c06baebd4edd8b702
Osama Bin Omar (bullhead) <m.osamabinomar@gmail.com>
2018-03-02 13:18:52 +0500

    login

---
+++ app/src/main/java/com/cleanapps/bullhead/drive/ui/login/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.cleanapps.bullhead.drive.ui.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cleanapps.bullhead.drive.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
